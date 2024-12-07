246e420d63145b24f92c9ab5366b7783b5f5c677
TemuulenBattulgaGit <temuulenbattulga@1209gmail.com>
2024-12-06 19:27:28 -0800

    train function

--- search/nn_classifier.py
+++ search/nn_classifier.py
@@ -1,8 +1,15 @@
 import numpy as np
+from sklearn.preproccessing import MinMaxScaler
+import time
 
 class NN_Classifier:
     def __init__(self):
         self.training_data = None
+        self.training_labels = None
+        #for normalizing feature values
+        self.scaler = MinMaxScaler()
 
-    def train(self, data):
-        self.training_data = data
+    def train(self, data, labels):
+        self.training_data = self.scaler.fit_transform(data)
+        #storing the labels as numpy array
+        self.training_labels = np.array(labels)
